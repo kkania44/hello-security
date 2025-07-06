6266d020ff9d44c35f38be6efd38287953feb423
Krzysztof Kania <kaniakrzysztof99@gmail.com>
2025-07-06 22:41:42 +0200

    Init repo

---
+++ src/main/java/example/hello_security/DefaultResource.java
@@ -0,0 +1,21 @@
+package example.hello_security;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class DefaultResource {
+
+    @GetMapping("/default/{param}")
+    public ResponseEntity<String> getDefault(@PathVariable String param) {
+        return ResponseEntity.ok("Default HTTP Response for param: [" + param + "]");
+    }
+
+    @GetMapping("/open")
+    public ResponseEntity<String> getOpen() {
+        return ResponseEntity.ok("Default HTTP Response on unsecured resource");
+    }
+
+}
---
+++ src/main/java/example/hello_security/SecurityConfig.java
@@ -0,0 +1,40 @@
+package example.hello_security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request ->
+                request
+                        .requestMatchers("/open").permitAll()
+                        .anyRequest().authenticated()
+        )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
